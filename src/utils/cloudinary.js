2c3dc6d96319b8e95da6c96d79571c666594aa88
Abhigyan <singhdefinite12@gmail.com>
2024-07-16 22:08:02 +0530

    production grade setup complete

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,37 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs'
+
+// Configuration
+  cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_API_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET  
+});
+
+ // Upload an image
+ const uploadOnCloudinary = async (localFilePath)=>{
+        try {
+            if(!localFilePath) return null
+
+          const response = await cloudinary.uploader.upload(localFilePath,{
+                resource_type :"auto"
+            })
+            // file has been uploades successfully
+            console.log("file has been uploaded sussessfully",response.url)
+            return response ;
+        } catch (error)  {
+                fs.unlinkSync(localFilePath) // remove the 
+                //locally saved temporary filr as the upload 
+                //operation got failed
+
+                return null
+
+            console.log(error);
+        }
+
+
+ }
+
+export {uploadOnCloudinary}
+ 
+
